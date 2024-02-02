74fa372332b2c2042caf8f0c5220b9a7a5052f06
Roma <romankechko98@yandex.ru>
2024-02-02 16:08:38 +0600

    fix: func = const NotFound

--- src/shared/pages/NotFound/NotFound.tsx
+++ src/shared/pages/NotFound/NotFound.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import {hot} from "react-hot-loader/root";
+import React, { FC } from "react";
+import { hot } from "react-hot-loader/root";
 
-function NotFoundComponent() {
-    return (
-        <div>
-            <h1>
-                error 404
-            </h1>
-            <span>
-                page not found
-            </span>
-        </div>
-    )
-}
+const NotFoundComponent: FC = () => {
+  return (
+    <div>
+      <h1>error 404</h1>
+      <span>page not found</span>
+    </div>
+  );
+};
 
 export const NotFound = hot(NotFoundComponent);
