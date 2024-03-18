cd5f27d2c2f1c645fa4ed236006087227647bf7a
Roma <romankechko98@yandex.ru>
2024-03-18 18:36:25 +0600

    feat: данные с сервера + отображение модалки

--- frontend/src/components/Product/Product.tsx
+++ frontend/src/components/Product/Product.tsx
@@ -8,16 +8,17 @@ import UrnComponent from "../Urn/Urn";
 import { IData } from "@/utils/interface";
 
 const ProductComponent: FC = () => {
-  const data = useAppSelector((state) => state.projects.projectsData);
+  const data: IData[] = useAppSelector(
+    (state) => state.projects?.projectsData as IData[]
+  );
   console.log(data);
-
   return (
     <>
       {data?.map((item: IData) => (
         <li className={style.list} key={item._id}>
           <Link href={`/${item._id} `} className={style.link}>
             <img
-              src={item.images.image_1}
+              src={`http://${item.images.image_1}`}
               alt={item.name}
               className={style.image}
               width={800}
