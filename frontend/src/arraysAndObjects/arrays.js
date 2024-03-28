eb62f2213bfa228f98b7adfb733d65dafae7b0c9
Roma <romankechko98@yandex.ru>
2024-03-28 11:08:24 +0600

    feat: добавил arrayFormFileInput

---
+++ frontend/src/arraysAndObjects/arrays.js
@@ -0,0 +1,44 @@
+export const arrayFormFileInput = [
+  {
+    title: 'Добавить видео',
+    id: 'video',
+    name: 'video',
+    accept: '.mp4'
+  },
+  {
+    title: 'Добавить gif',
+    id: 'gif',
+    name: 'gif',
+    accept: '.gif'
+  },
+  {
+    title: 'Добавить картинку гифки',
+    id: 'gif-image',
+    name: 'gif-image',
+    accept: '.jpeg, .png'
+  },
+  {
+    title: `Добавить основное изображение`,
+    id: 'image_1',
+    name: 'image_1',
+    accept: '.jpeg, .png'
+  },
+  {
+    title: 'Добавить второе изображение',
+    id: 'image_2',
+    name: 'image_2',
+    accept: '.jpeg, .png'
+  },
+  {
+    title: 'Добавить третье изображение',
+    id: 'image_3',
+    name: 'image_3',
+    accept: '.jpeg, .png'
+  },
+  {
+    title: 'Добавить pdf',
+    id: 'pdf',
+    name: 'pdf',
+    accept: '.pdf'
+  }
+]
--- frontend/src/components/form/formFileInputs/formFileInputs.tsx
+++ frontend/src/components/form/formFileInputs/formFileInputs.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FormFileInput from '@/components/form/formFileInput/formFileInput'
 import { TProjectData } from '@/utils/type'
-
+import { arrayFormFileInput } from '@/arraysAndObjects/arrays'
 interface IFormFileInputProps {
   setProjectData: React.Dispatch<React.SetStateAction<TProjectData>>
   projectData: TProjectData
@@ -13,68 +13,16 @@ export default function FormFileInputs ({
 }: IFormFileInputProps) {
   return (
     <>
-      <FormFileInput
-        title={'Добавить видео'}
-        id={'video'}
-        name={'video'}
-        accept={'.mp4'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={'Добавить gif'}
-        id={'gif'}
-        name={'gif'}
-        accept={'.gif'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={'Добавить картинку гифки'}
-        id={'gif-image'}
-        name={'gif-image'}
-        accept={'.jpeg, .png'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={`Добавить основное изображение`}
-        id={'image_1'}
-        name={'image_1'}
-        accept={'.jpeg, .png'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={'Добавить второе изображение'}
-        id={'image_2'}
-        name={'image_2'}
-        accept={'.jpeg, .png'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={'Добавить третье изображение'}
-        id={'image_3'}
-        name={'image_3'}
-        accept={'.jpeg, .png'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
-
-      <FormFileInput
-        title={'Добавить pdf'}
-        id={'pdf'}
-        name={'pdf'}
-        accept={'.pdf'}
-        setProjectData={setProjectData}
-        projectData={projectData}
-      />
+      {arrayFormFileInput.map(item => (
+        <FormFileInput
+          title={item.title}
+          id={item.id}
+          name={item.name}
+          accept={item.accept}
+          setProjectData={setProjectData}
+          projectData={projectData}
+        />
+      ))}
     </>
   )
 }
